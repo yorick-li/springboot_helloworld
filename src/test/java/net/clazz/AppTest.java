e5b2aa0079d3d741600b2a195a8811cb11a030c2
YorickLi <yorick_li2017@163.com>
2018-11-11 16:56:15 +0800

    Initial commit

---
+++ src/test/java/net/clazz/AppTest.java
@@ -0,0 +1,20 @@
+package net.clazz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HelloWorld.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
